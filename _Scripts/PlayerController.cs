fc539b19db11c8b52dacbfff029405cd552cfb2f
SalteWalte <61295061+WalterKeiler@users.noreply.github.com>
2024-10-24 22:58:15 -0400

    Movement controls

--- _Scripts/PlayerController.cs
+++ _Scripts/PlayerController.cs
@@ -4,6 +4,8 @@ using System;
 public partial class PlayerController : Node3D
 {
 	[Export] float speed = 5;
+
+	Vector3 moveDirection;
 	
 	// Called when the node enters the scene tree for the first time.
 	public override void _Ready()
@@ -13,7 +15,42 @@ public partial class PlayerController : Node3D
 	// Called every frame. 'delta' is the elapsed time since the previous frame.
 	public override void _Process(double delta)
 	{
-		if(Input.IsKeyPressed(Key.W))
-			Translate(Vector3.Forward * (float)delta * speed);
+		GetInput();
+		Move(delta);
+	}
+	
+	void GetInput()
+	{
+		if (Input.IsKeyPressed(Key.W))
+		{
+			moveDirection.Z = 1;
+		}
+		else if (Input.IsKeyPressed(Key.S))
+		{
+			moveDirection.Z = -1;
+		}
+		else
+		{
+			moveDirection.Z = 0;
+		}
+		if (Input.IsKeyPressed(Key.A))
+		{
+			moveDirection.X = 1;
+		}
+		else if (Input.IsKeyPressed(Key.D))
+		{
+			moveDirection.X = -1;
+		}
+		else
+		{
+			moveDirection.X = 0;
+		}
+
+		moveDirection.Normalized();
+	}
+
+	void Move(double delta)
+	{
+		Translate(moveDirection * (float)delta * speed);
 	}
 }
